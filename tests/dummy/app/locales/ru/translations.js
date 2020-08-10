c6361838e6c06589169d8d548c391984db685ac4
Daniil Pavlov <dan43575@gmail.com>
2020-08-10 16:40:10 +0500

    Implemented flexberry-tabs
    - Added ability to scroll tabs with mousewheel or by dragging the tabs;
    - In case tabs overflow its parent container, a dropdown appears, from which another tab can be chosen without scrolling;
    - Implemented scrolling with mousewheel, ability to select new tab from dropdown;
    
    LESS refactoring
    
    
    fix
    
    
    Refactoring
    
    
    JsHint fixes
    
    
    Fix for passing jscs

--- tests/dummy/app/locales/ru/translations.js
+++ tests/dummy/app/locales/ru/translations.js
@@ -652,6 +652,14 @@ Ember.$.extend(true, translations, {
               'title': ''
             }
           },
+          'flexberry-tab-bar': {
+            'caption': 'flexberry-tab-bar',
+            'title': '',
+            'settings-example': {
+              'caption': 'Пример работы с настройками',
+              'title': ''
+            }
+          },
           'flexberry-text-cell': {
             'caption': 'flexberry-text-cell',
             'title': '',
@@ -1149,6 +1157,11 @@ Ember.$.extend(true, translations, {
       'flexberry-simpledatetime': {
         'settings-example': {
           'caption': 'Flexberry-simpledatetime. Пример работы с настройками'
+        }
+      },
+      'flexberry-tab-bar': {
+        'settings-example': {
+          'caption': 'Flexberry-tab-bar. Пример работы с настройками'
         }
       },
       'flexberry-text-cell': {
